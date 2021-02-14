62c55d9738936d5dddb24fad4b2145bc58e31344
Jarne-Theys <jarne.theys@student.ucll.be>
2021-02-14 13:34:40 +0100

    01-06-02

--- 01-basic-python/06-tuples/02-count-turns/student.py
+++ 01-basic-python/06-tuples/02-count-turns/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+def count_turns(ns):
+    counter = 0
+    for (x, y) in zip(ns, ns[1:]):
+        if counter % 2 == 0 and x > y:
+            counter += 1
+        if counter % 2 != 0 and x < y:
+            counter += 1
+    return counter
