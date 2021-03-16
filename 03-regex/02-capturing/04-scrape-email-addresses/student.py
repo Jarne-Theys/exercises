4cfa9d62e31619c4a3468a0f7ff47c37c06fe499
Jarne-Theys <jarne.theys@student.ucll.be>
2021-03-16 16:06:47 +0100

    Regexes compleet

--- 03-regex/02-capturing/04-scrape-email-addresses/student.py
+++ 03-regex/02-capturing/04-scrape-email-addresses/student.py
@@ -1,1 +1,6 @@
 # Write your code here
+# re.fullmatch(r'[a-z0-9.]+@(student\.)?ucll\.be',string)
+import re
+def scrape_email_addresses(string):
+    list = re.findall(r'([\w\d.]+@[\w.]+)',string)
+    return list
--- 03-regex/03-substituting/04-hide-email-addresses/student.py
+++ 03-regex/03-substituting/04-hide-email-addresses/student.py
@@ -1,1 +1,7 @@
 # Write your code here
+import re
+def hide_email_addresses(string):
+    def replace_with_asterisk(match):
+        return len(match.group(0))*"*"
+
+    return re.sub(r'([\w\d.]+@[\w.]+)',replace_with_asterisk,string)
